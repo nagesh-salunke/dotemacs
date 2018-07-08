cf5083b8ebfd7ca60d652f9768f6388916aafb2d
nagesh-salunke <salunkenagesh14@gmail.com>
2018-07-08 12:59:54 +0530

    Use fish shell as I have been using it for a while

--- configs/personal-multi-term.el
+++ configs/personal-multi-term.el
@@ -1,10 +1,10 @@
 ;; multi-term
 
-(setq multi-term-program "/bin/bash -l")
+(setq multi-term-program "/usr/local/bin/fish -l")
 
 (when (require 'multi-term nil t)
   (global-set-key (kbd "<s-return>") 'multi-term)
   (setq multi-term-buffer-name "term"
-        multi-term-program "/bin/bash"))
+        multi-term-program "/usr/local/bin/fish"))
 
 (provide 'personal-multi-term)
