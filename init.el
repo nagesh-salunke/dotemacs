1dcf58d040e5350e7eed49fc377c740dbb9d16de
nagesh-salunke <salunkenagesh14@gmail.com>
2018-08-11 12:28:19 +0530

    Moving my config to org style.

--- init.el
+++ init.el
@@ -10,45 +10,15 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-;;; configure use package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(setq use-package-verbose t)
-(setq use-package-always-ensure t)
-
-(require 'package)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+(defun display-startup-echo-area-message ()
+  (message "Hello! How are you today, %s" (user-login-name)))
 
 (setq custom-file "~/.emacs.d/custom-settings.el")
 (load custom-file t)
 
-;;; personal information
-(defun display-startup-echo-area-message ()
-  (message "Hello! How are you today, %s" (user-login-name)))
-
-(setq user-full-name "Nagesh Salunke"
-      user-mail-address "salunkenagesh14@gmail.com"
-      calendar-latitude 18.5204
-      calendar-longitude 73.8567
-      calendar-location-name "Pune, IN")
+;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
 ;;show tip of the day
 (totd)
-
-(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
-  (normal-top-level-add-subdirs-to-load-path))
-
-(defvar root-dir "~/.emacs.d")
-(defvar configs-dir (concat root-dir "/configs"))
-
-;;fancy lambda mode
-(global-prettify-symbols-mode t)
-
-;; load configs
-(add-to-list 'load-path configs-dir)
-(require 'personal-multi-term)
-(require 'personal-settings)
-(require 'personal-org)
