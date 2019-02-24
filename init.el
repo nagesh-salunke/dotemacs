7738b5f07bb28472af01e341e2ee600dfdfa4c50
Nagesh Salunke <nagesh.salunke@taranawireless.com>
2019-02-24 21:07:58 +0530

    Change to config  - cleaning up few things :D. A lot of things have changed since last this was updated.

--- init.el
+++ init.el
@@ -16,12 +16,13 @@
 (setq custom-file "~/.emacs.d/custom-settings.el")
 (load custom-file t)
 
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(setq package-check-signature nil)
 ;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
 ;;show tip of the day
 (totd)
